b5178aa321a77916f2ef0376347ece81b2a242ce
Nadhir <mamokaze@gmail.com>
2021-06-16 06:18:05 +0100

    Fix: items array won't change unless you save

--- public/js/items.js
+++ public/js/items.js
@@ -38,7 +38,7 @@ const x = ' <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill=
 const y = ' <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M13.485 1.431a1.473 1.473 0 0 1 2.104 2.062l-7.84 9.801a1.473 1.473 0 0 1-2.12.04L.431 8.138a1.473 1.473 0 0 1 2.084-2.083l4.111 4.112 6.82-8.69a.486.486 0 0 1 .04-.045z"/></svg>'
 
 function click(id, add = false) {
-	let item = add ? { name: '', price: 0, inStock: true } : [...items].find(e => e.id == id);
+	let item = add ? { name: '', price: 0, inStock: true } : { ...items.find(e => e.id == id) };
 
 	$('#modal-label').html(add ? l[lng].add_item : item.name);
 	$('#item-name').val(item.name);
