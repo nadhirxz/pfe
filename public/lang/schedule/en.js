c9f78b4614e9fec3ff361215c548e91ae178bce7
Nadhir <mamokaze@gmail.com>
2021-06-21 10:45:51 +0100

    Fix for admin schedule

---
+++ public/lang/schedule/en.js
@@ -0,0 +1,12 @@
+let lang_text = {
+	wrkn: "Working Now",
+	not_wrkn: "NOT Working Now",
+	stop_wrkn: "Stop working",
+	start_wrkn: "Start working",
+	save: "Save",
+	sure_stop: "Are you sure you want to stop working ?",
+	sure_start: "Are you sure you want to start working ?",
+	stop: "Stop",
+	start: "Start",
+	cancel: "Cancel"
+}
---
+++ public/lang/schedule/fr.js
@@ -0,0 +1,12 @@
+let lang_text = {
+	wrkn: "On travaille maintenant",
+	not_wrkn: "On ne travaille PAS maintenant",
+	stop_wrkn: "Arrêter de travailler",
+	start_wrkn: "Commencer à travailler",
+	save: "Sauvegarder",
+	sure_stop: "Êtes-vous sûr de vouloir arrêter de travailler ?",
+	sure_start: "Êtes-vous sûr de vouloir commencer à travailler ?",
+	stop: "Arrêter",
+	start: "Commencer",
+	cancel: "Annuler"
+}
