b1a7979c5d510a58a17cceedfb56d1d35b653df2
Nadhir <mamokaze@gmail.com>
2021-06-23 15:16:46 +0100

    Deleted/Updated some variables

--- data.js
+++ data.js
@@ -11,27 +11,24 @@ let settings = {
 	maxPinRetries: 5,
 
 	maxNameLength: 40, // 40 characters
-	maxHoursWithoutConfirmation: 50, // hours .. obviously
+	maxHoursWithoutConfirmation: 48, // hours
 
 	partnerPercentage: 10,
 	driverPercentage: 25,
 
 	maxDeliveryDistance: 60, // km
-	maxDriverDeliveriesAtOnce: 5,
-	driverSpeed: 25, // km/h
+	driverSpeed: 35, // km/h
 	driverRestTime: 5, // minutes
 	usersSocketTimeout: 20000, // ms
 
 	intervalBetweenDeliveries: 5, // minutes
-
-	deliveriesToBeQualified: 5,
 
 	AlgiersPos: [36.68686093982163, 3.0768628983554978],
 
 	nearestMinute: 5,
 
 	allowedImgExt: ['png', 'jpg', 'jpeg', 'gif'],
-	partnerImgSize: 300
+	partnerImgSize: 500
 }
 
 let titles = {
@@ -45,14 +42,12 @@ let titles = {
 		too_many_requests: "Too many requests",
 		plz_wait_at_least: `Please wait at least ${settings.defaultMinIntervalBetweenDeliveries} minutes before submitting a new delivery.`,
 		new_buy: "New Buy",
-		long_distance_delivery: "Long distance delivery",
 		login: "Login",
 		register: "Register",
 		partners_reg: "Partners Registration",
 		confirm: "Confirm",
 		terms: "Terms of Service",
 		driver: "Driver",
-		car_driver: "Car Driver",
 		drivers: "Drivers",
 		admin: "Admin",
 		add_partner: "Add Partner",
@@ -62,13 +57,10 @@ let titles = {
 		pg_dsnt_xst: "Page doesn't exist",
 		error: "Error",
 		a_dlvr_err_hppnd: "An error happened. Please try submitting your delivery later.",
-		ur_ldds: "Your long distance deliveries",
 		delivery: "Delivery",
 		about: "About",
 		profile: "Profile",
 		privacy: "Privacy Policy",
-		blacklist: "Blacklist",
-		edit_phone: "Edit Phone Number",
 		edit_password: "Edit Password",
 		partners: "Partners",
 		your_items: "Your Items"
@@ -83,14 +75,12 @@ let titles = {
 		too_many_requests: "Trop de demandes",
 		plz_wait_at_least: `Veuillez attendre au moins ${settings.defaultMinIntervalBetweenDeliveries} minutes avant de soumettre une nouvelle livraison.`,
 		new_buy: "Nouvel achat",
-		long_distance_delivery: "Livraison longue distance",
 		login: "S'identifier",
 		register: "S'inscrire",
 		partners_reg: "Inscription des partenaires",
 		confirm: "Confirmer",
 		terms: "Conditions d'utilisation",
 		driver: "Livreur",
-		car_driver: "Livreur avec voiture",
 		drivers: "Livreurs",
 		admin: "Administrateur",
 		add_partner: "Ajouter un partenaire",
@@ -100,13 +90,10 @@ let titles = {
 		pg_dsnt_xst: "La page n'existe pas",
 		error: "Erreur",
 		a_dlvr_err_hppnd: "Une erreur s'est produite. Veuillez essayer de soumettre votre livraison plus tard.",
-		ur_ldds: "Vos livraisons longue distance",
 		delivery: "Livraison",
 		about: "À propos",
 		profile: "Profil",
 		privacy: "Politique de confidentialité",
-		blacklist: "Liste Noire",
-		edit_phone: "Modifier le Numéro de Téléphone",
 		edit_password: "Modifier le Mot de Passe",
 		partners: "Partenaires",
 		your_items: "Votre Objets"
@@ -121,14 +108,12 @@ let titles = {
 		too_many_requests: "طلباتك كثيرة ",
 		plz_wait_at_least: `الرجاء انتظار ${settings.defaultMinIntervalBetweenDeliveries} دقائق على الأقل قبل تقديم طلب توصيل جديد.`,
 		new_buy: "طلب شراء",
-		long_distance_delivery: "توصيل للمسافات طويلة",
 		login: "تسجيل الدخول",
 		register: "تسجيل",
 		partners_reg: "تسجيل الشركاء",
 		confirm: "تأكيد",
 		terms: "شروط الخدمة",
 		driver: "سائق",
-		car_driver: "سائق سيارة",
 		drivers: "السائقون",
 		admin: "مشرف",
 		add_partner: "إضافة شريك",
@@ -138,13 +123,10 @@ let titles = {
 		pg_dsnt_xst: "الصفحة غير موجودة",
 		error: "خطأ",
 		a_dlvr_err_hppnd: "حدث خطأ. يرجى محاولة تقديم طلب التوصيل الخاص بك في وقت لاحق.",
-		ur_ldds: "طلبات التسليم للمسافات الطويلة",
 		delivery: "طلب التوصيل",
 		about: "حول",
 		profile: "الملف الشخصي",
 		privacy: "سياسة الخصوصية",
-		blacklist: "القائمة السوداء",
-		edit_phone: "تغيير رقم الهاتف",
 		edit_password: "تغيير كلمة السر",
 		partners: "الشركاء",
 		your_items: "السلعة الخاصة بك"
