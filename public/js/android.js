709a6ae769c8a28179907635baebe20d13531136
Nadhir <mamokaze@gmail.com>
2021-06-23 14:56:19 +0100

    Fix

--- public/js/android.js
+++ public/js/android.js
@@ -2,7 +2,7 @@ if (typeof (Android) != 'undefined' && Android.getAppVersion().endsWith('(Driver
 	$('.bg-success').each((i, e) => {
 		$(e).addClass('bg-primary').removeClass('bg-primary');
 	})
-	$('#nav-reg-btn').attr('href', '/staff')
+	$('#nav-reg-btn').attr('href', '/drivers')
 	$('.btn .btn .btn-success .btn-block').each((i, e) => {
 		$(e).addClass('btn-primary').removeClass('btn-success');
 	});
