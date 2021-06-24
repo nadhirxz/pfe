bffc161b6f155f81580f249f2f74cf3e28e9a10d
Nadhir <mamokaze@gmail.com>
2021-06-24 02:26:59 +0100

    Changed style of driver's version of the website

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,5 +1,7 @@
-$(document).ready(function () {
-    $('[data-toggle="offcanvas"], #navToggle').on('click', function () {
-        $('.offcanvas-collapse').toggleClass('open')
-    })
-});
+$(document).ready(() => $('[data-toggle="offcanvas"], #navToggle').on('click', () => $('.offcanvas-collapse').toggleClass('open')));
+
+if ($('.navbar').hasClass('bg-dark')) {
+	$('.btn').each(e => {
+		$(e).addClass('btn-dark').removeClass('btn-info');
+	});
+}
