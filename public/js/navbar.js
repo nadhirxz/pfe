0c052742a1939d3d4c6ab59ecf2eea1371c055c0
Nadhir <mamokaze@gmail.com>
2021-06-27 09:33:09 +0100

    Changed navbar colors

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,7 +1,5 @@
 $(document).ready(() => $('[data-toggle="offcanvas"], #navToggle').on('click', () => $('.offcanvas-collapse').toggleClass('open')));
 
 if ($('.navbar').hasClass('bg-dark')) {
-	$('.btn-info').each((i, e) => {
-		$(e).addClass('btn-dark').removeClass('btn-info');
-	});
+	$('.btn-c').each((i, e) => $(e).addClass('btn-dark').removeClass('btn-c'));
 }
