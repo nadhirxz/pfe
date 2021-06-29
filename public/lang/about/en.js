e18d29e364e3f7b9b0b8802695a462b4bb08ef8b
Nadhir <mamokaze@gmail.com>
2021-06-29 16:44:36 +0100

    Fixed about page

--- public/lang/about/en.js
+++ public/lang/about/en.js
@@ -1,15 +1,8 @@
 lang_text = {
-    desc_0_text: "With Wesseli, the world comes to you !",
-    desc_1_text: "Wesseli is a delivery application available in Chlef that allows you to receive all your orders at home, in record time and at an unbeatable price; whether it's food, coffee, accessories or even taking care of repairing your glasses and much more. Best of all, you can choose the delivery method that best suits your needs.",
-    desc_2_text: "Forget all the fatigue of shopping, Wesseli makes your job as easy as possible. Isn't life with Wesseli good ?",
-    conatct_text: "Contact",
-    attribution_text: "Attribution",
-    ldng_img_by_text: "Loading image is provided by",
-    fvcn_by_text: "Favicon is provided by",
-    terms_text: "Terms of Service",
-    privacy_text: "Privacy Policy",
-    winners_text: "Winners this week :"
+	conatct_text: "Contact",
+	terms_text: "Terms of Service",
+	privacy_text: "Privacy Policy",
 }
 if (typeof (Android) != 'undefined' && typeof (Android.getAppVersion()) != 'undefined' && typeof (Android.getAppReleaseDate()) != 'undefined') {
-    lang_text.attribution_text = "About the app";
+	lang_text.attribution_text = "About the app";
 }
