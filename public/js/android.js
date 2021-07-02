f8b0ddacd82f5d4a49024d4e3b13775597fcc451
Nadhir <mamokaze@gmail.com>
2021-07-02 21:03:32 +0100

    Fixed button classes for driver app

--- public/js/android.js
+++ public/js/android.js
@@ -1,7 +1,6 @@
 if (typeof (Android) != 'undefined' && Android.getAppVersion().endsWith('(Driver)')) {
-	$('.bg-c').each((i, e) => {
-		$(e).addClass('bg-dark').removeClass('bg-c');
-	})
+	$('.bg-c').each((i, e) => $(e).addClass('bg-dark').removeClass('bg-c'));
+	$('.btn-info').each((i, e) => $(e).addClass('btn-dark').removeClass('btn-c'));
 	$('#nav-reg-btn').attr('href', '/drivers')
 	$('.btn .btn .btn-c .btn-block').each((i, e) => {
 		$(e).addClass('btn-dark').removeClass('btn-c');
