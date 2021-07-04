1652b0292d8ef1839d06a20348e6c4012a4b404d
Nadhir <mamokaze@gmail.com>
2021-07-04 23:06:34 +0100

    Fix

--- public/lang/admin/ar.js
+++ public/lang/admin/ar.js
@@ -1,7 +1,7 @@
 lang_text = {
     deliveries_today: "توصيلات اليوم : ",
     profit_today: "أرباح اليوم : ",
-    details: "تفاصيل",
+    details: "التفاصيل",
     shcedule: "جدول الأوقات",
     new_key: "مفتاح سري جديد",
     shops: "المتاجر"
